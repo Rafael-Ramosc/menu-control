8e8e64b0dc74f0401a39ae65d2f8c009e811aad0
Rafael Ramos <rafael.ramosrc@gmail.com>
2024-06-25 12:08:08 -0300

    refactor: menu functionality and user creation

--- src/menu.rs
+++ src/menu.rs
@@ -1,4 +1,5 @@
 pub mod user;
+use std::fmt::Display;
 
 use crate::menu::user::User;
 
@@ -19,14 +20,9 @@ pub fn select_option() -> i32 {
 pub fn option_control(option: i32) {
     match option {
         1 => {
-            println!(" ------- Creating new user -------");
-            println!("Enter your user name: ");
-            let mut user_name_select = String::new();
-            std::io::stdin().read_line(&mut user_name_select).unwrap();
-            let user = create_user(user_name_select);
-
+            let user = create_user();
             let user_name = User::get_username(user);
-            println!("User {} created ", user_name);
+            println!(" \\\\\\ User {} Created! ///", red(user_name));
         }
         2 => println!("My name is Rafael Ramos"),
         3 => println!("Exiting..."),
@@ -34,7 +30,25 @@ pub fn option_control(option: i32) {
     }
 }
 
-fn create_user(user_name_select: String) -> User {
+fn create_user() -> User {
+    println!(" ------- Creating new user -------");
+
+    println!("Enter your user name: ");
+    let mut user_name_select = String::new();
+    std::io::stdin().read_line(&mut user_name_select).unwrap();
     let user_name_select = user_name_select.trim().to_string();
+
     User::new(0, user_name_select)
+}
+
+pub fn bold<T: Display>(text: T) -> String {
+    format!("\x1b[1m{}\x1b[0m", text)
+}
+
+pub fn color<T: Display>(text: T, color_code: &str) -> String {
+    format!("\x1b[{}m{}\x1b[0m", color_code, text)
+}
+
+pub fn red<T: Display>(text: T) -> String {
+    color(text, "31")
 }
