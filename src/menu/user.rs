a14b5bf067e8e26168ee22bba2bddcf25f44a790
Rafael-Ramosc <rafael.ramosrc@gmail.com>
2024-06-25 09:30:35 -0300

    refactor: Add User struct and Preferences struct to menu/user.rs

---
+++ src/menu/user.rs
@@ -0,0 +1,15 @@
+pub struct User {
+    pub id: i32,
+    pub user_name: String,
+    // pub dat_create: Date,
+    // pub preferences: Preferences,
+}
+
+struct Preferences {
+    font_color: String,
+    font_size: i32,
+}
+
+// impl User {
+//     pub fn new(id: i32, user_name: String) -> User {}
+// }
