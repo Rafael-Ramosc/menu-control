4c8f59cc10eca79e97806713fdf513e668deaa16
Rafael Ramos <rafael.ramosrc@gmail.com>
2024-06-27 10:47:53 -0300

    feat: Add serde_json dependency and update menu functionality

--- src/menu/user.rs
+++ src/menu/user.rs
@@ -1,28 +1,31 @@
 pub struct User {
     id: i32,
     user_name: String,
-    // pub dat_create: Date,
-    // pub preferences: Configuration,
-}
-
-struct Configuration {
-    font_color: String,
-    font_size: i32,
+    pub preferences: Configuration,
 }
 
 impl User {
     pub fn new(id: i32, user_name: String) -> Self {
-        Self { id, user_name }
-    }
-
-    pub fn default() -> User {
-        User {
-            id: 0,
-            user_name: String::from("default_profile"),
+        Self {
+            id,
+            user_name,
+            preferences: Configuration::default(),
         }
     }
 
     pub fn get_username(self) -> String {
         self.user_name
+    }
+}
+
+struct Configuration {
+    user_name_color: String,
+}
+
+impl Configuration {
+    pub fn default() -> Configuration {
+        Configuration {
+            user_name_color: "RED".to_string(),
+        }
     }
 }
